d7a0086b17cfaa31ea6fd60800808002c46d6900
alexander <alexander@debian>
2016-12-19 13:46:18 +0300

    Add Django skeleton

---
+++ blog/blog/debug.py
@@ -0,0 +1,15 @@
+from .common import *
+
+
+DEBUG = True
+
+SECRET_KEY = 'rt$2t!lnn%&*$vf4^!&ypd=(tt+u5mu072-voxt@b@1t)2@kwb'
+
+ALLOWED_HOSTS = ['127.0.0.1', ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
