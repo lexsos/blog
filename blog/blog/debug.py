a751d57110d53bfd2d8e8994467f0bc702a8af9d
alexander <alexander@debian>
2016-12-19 13:56:29 +0300

    Add debug toolbar

--- blog/blog/debug.py
+++ blog/blog/debug.py
@@ -6,6 +6,7 @@ DEBUG = True
 SECRET_KEY = 'rt$2t!lnn%&*$vf4^!&ypd=(tt+u5mu072-voxt@b@1t)2@kwb'
 
 ALLOWED_HOSTS = ['127.0.0.1', ]
+INTERNAL_IPS = ['127.0.0.1', ]
 
 DATABASES = {
     'default': {
@@ -13,3 +14,11 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
