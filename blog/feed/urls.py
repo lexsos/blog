b0839a42454fb27e993d679bcb5cc42335a4ee55
alexander <alexander@debian>
2016-12-20 10:51:53 +0300

    Add post create page

--- blog/feed/urls.py
+++ blog/feed/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
-from .views import FeedView
+from .views import FeedView, PostCreate
 
 
 urlpatterns = [
     url(r'^feed/$', FeedView.as_view(), name='blog_feed'),
+    url(r'^new_post/$', PostCreate.as_view(), name='blog_create_post'),
+    url(r'^login/$', auth_views.login, {'template_name': 'feed/login.html'}, name='login'),
 ]
--- blog/feed/views.py
+++ blog/feed/views.py
@@ -1,14 +1,40 @@
 from django.views.generic.list import ListView
+from django.views.generic.edit import FormView
+from django.core.urlresolvers import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .models import Feed
+from .models import Feed, Post
+from .forms import PostCreateForm
 
 
-class FeedView(ListView):
+class FeedView(LoginRequiredMixin, ListView):
 
     model = Feed
 
-    def get_queryset(self, *args, **kwargs):
-        queryset = super(FeedView, self).get_queryset(*args, **kwargs)
+    def get_queryset(self):
+        queryset = super(FeedView, self).get_queryset()
         if self.request.user.is_authenticated():
             queryset = queryset.filter(subscriber=self.request.user)
         return queryset
+
+    def get_login_url(self):
+        return reverse('login')
+
+
+class PostCreate(LoginRequiredMixin, FormView):
+
+    template_name = 'feed/post_create.html'
+    form_class = PostCreateForm
+
+    def get_success_url(self):
+        return reverse('blog_feed')
+
+    def get_login_url(self):
+        return reverse('login')
+
+    def form_valid(self, form):
+        if self.request.user.is_authenticated():
+            post = form.save(commit=False)
+            post.author = self.request.user
+            post.save()
+        return super(PostCreate, self).form_valid(form)
